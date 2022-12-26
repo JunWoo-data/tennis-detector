3256bbcd2478663015afb244083bb6d76941423d
root <youngjun.woo.data@gmail.com>
2022-12-26 03:16:06 +0000

    skip if clip already exists

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 # %%
-!sudo pip3 install imageio==2.4.1
+# !sudo pip3 install imageio==2.4.1
 
 # %%
 from moviepy.editor import VideoFileClip
@@ -29,7 +29,6 @@ def video_to_clip(youtube_link, season, match_date, court_number, match_number, 
     video_path = base_path + "match_video.mp4"
     clip = VideoFileClip(video_path)
 
-    clip_number = 1
     for i in range(len(clip_info_list)):
         clip_info = clip_info_list[i]
 
@@ -40,15 +39,19 @@ def video_to_clip(youtube_link, season, match_date, court_number, match_number, 
         end_sec = int(end_string.split(":")[0]) * 60 + int(end_string.split(":")[1]) * 1
 
         
-        clip_path = base_path + "clip" + str(clip_number) + "/"
+        clip_path = base_path + "clip" + str(i + 1) + "/"
 
         if not os.path.exists(clip_path):
           os.makedirs(clip_path)
-
-        clip.subclip(start_sec, end_sec).write_videofile(clip_path + "clip" + str(clip_number) + ".mp4")
         
-        clip_number += 1
-    
+        clip_file_path = clip_path + "clip" + str(i + 1) + ".mp4"
+        
+        if os.path.exists(clip_file_path):
+            continue
+        
+        else:
+            clip.subclip(start_sec, end_sec).write_videofile(clip_file_path)
+        
     os.remove(video_path)
 # %%
 def video_to_frames(youtube_link, season, match_date, court_number, match_number):
@@ -101,9 +104,31 @@ clip_info_list = [["0:9", "0:21"], ["0:43", "0:58"], ["1:11", "1:23"], ["1:38", 
 ["11:00", "11:15"], ["11:31", "11:40"], ["11:53", "12:01"], ["13:06", "13:17"], ["13:30", "13:42"], ["13:52", "14:25"], ["14:39", "14:56"], ["15:09", "15:15"],
 ["15:36", "15:56"], ["16:15", "16:23"], ["16:41", "16:47"], ["17:00", "17:14"], ["17:37", "17:42"], ["17:57", "18:08"], ["18:31", "18:37"], ["18:49", "18:55"],
 ["19:06", "19:25"], ["19:40", "19:54"], ["20:10", "20:18"], ["20:36", "20:40"], ["20:53", "21:05"], ["21:16", "21:22"], ["21:32", "21:48"], ["22:00", "22:10"],
-["22:23", "22:37"], ["23:13", "23:32"], ["24:00", "24:07"], ["24:22", "24:29"], ["24:43", "24:57"], ["25:10", "25:18"], ["25:53", "25:51"], ["26:58", "27:20"],
+["22:23", "22:37"], ["23:13", "23:32"], ["24:00", "24:07"], ["24:22", "24:29"], ["24:43", "24:57"], ["25:10", "25:18"], ["25:33", "25:51"], ["26:58", "27:20"],
 ["27:50", "27:59"], ["28:13", "28:35"], ["28:53", "28:59"], ["29:16", "29:21"], ["29:39", "29:46"], ["29:57", "30:07"], ["30:27", "30:34"]]
 video_to_clip("https://youtu.be/AdmCmegtgc8", "22F", "20220908", "court1", "match1", clip_info_list)
+
+
+# %%
+def get_maximum_clip(season, match_date, court_number, match_number):
+    base_path = DATA_PATH + "detect/" + season + "/" + match_date + "/" + court_number + "/" + match_number + "/"
+    
+    return len(os.listdir(base_path))
+   
+# %%
+clip_file_path = base_path + "clip" + str(1) + "/" + "clip" + str(1) + ".mp4"
+os.path.exists(clip_file_path)
+   
+# %%
+season = "22F"
+match_date = "20220908"
+court_number = "court1"
+match_number = "match1"
+base_path = DATA_PATH + "detect/" + season + "/" + match_date + "/" + court_number + "/" + match_number + "/" 
+base_path
+
+# %%
+
 
 # %%
 for i in range(len(clip_info_list)):
