e726e74903a2381fb25dc6cdafd1c526c063994b
root <youngjun.woo.data@gmail.com>
2022-12-26 02:49:35 +0000

    fix video to clip function

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
 # %%
+!sudo pip3 install imageio==2.4.1
+
+# %%
 from moviepy.editor import VideoFileClip
 import pytube
 from pytube import YouTube
@@ -7,10 +10,11 @@ import os
 from google.colab.patches import cv2_imshow
 from google.colab import drive
 import numpy as np
+import glob.glob
 from config import DATA_PATH
 
 # %%
-def video_to_clip(youtube_link, match_date, court_number, match_number, clip_list):
+def video_to_clip(youtube_link, season, match_date, court_number, match_number, clip_info_list):
     # make folder if not exists
     base_path = DATA_PATH + "detect/" + season + "/" + match_date + "/" + court_number + "/" + match_number + "/"
     
@@ -25,23 +29,27 @@ def video_to_clip(youtube_link, match_date, court_number, match_number, clip_lis
     video_path = base_path + "match_video.mp4"
     clip = VideoFileClip(video_path)
 
-    for i in range(len(clip_list)):
-        clip = clip_list[i]
+    clip_number = 1
+    for i in range(len(clip_info_list)):
+        clip_info = clip_info_list[i]
 
-        start_string = clip[0]
+        start_string = clip_info[0]
         start_sec = int(start_string.split(":")[0]) * 60 + int(start_string.split(":")[1]) * 1
 
-        end_string = clip[1]
+        end_string = clip_info[1]
         end_sec = int(end_string.split(":")[0]) * 60 + int(end_string.split(":")[1]) * 1
 
-        clip_number = 1
+        
         clip_path = base_path + "clip" + str(clip_number) + "/"
 
         if not os.path.exists(clip_path):
           os.makedirs(clip_path)
 
         clip.subclip(start_sec, end_sec).write_videofile(clip_path + "clip" + str(clip_number) + ".mp4")
-
+        
+        clip_number += 1
+    
+    os.remove(video_path)
 # %%
 def video_to_frames(youtube_link, season, match_date, court_number, match_number):
     # make folder if not exists
@@ -86,7 +94,26 @@ def video_to_frames(youtube_link, season, match_date, court_number, match_number
     # delete downloaded video because we only need frames from video
     os.remove(video_path)
 
+# %%
+clip_info_list = [["0:9", "0:21"], ["0:43", "0:58"], ["1:11", "1:23"], ["1:38", "1:50"], ["2:05", "2:10"], ["2:25", "2:31"], ["2:48", "2:56"], ["3:09", "3:17"],
+["3:33", "3:43"], ["3:55", "4:00"], ["4:22", "4:27"], ["4:45", "4:53"], ["5:09", "5:14"], ["5:31", "5:35"], ["5:59", "6:07"], ["6:21", "6:27"],
+["6:40", "6:46"], ["6:57", "7:10"], ["7:22", "7:36"], ["7:54", "8:08"], ["8:30", "8:56"], ["9:13", "9:35"], ["9:47", "10:04"], ["10:33", "10:48"],
+["11:00", "11:15"], ["11:31", "11:40"], ["11:53", "12:01"], ["13:06", "13:17"], ["13:30", "13:42"], ["13:52", "14:25"], ["14:39", "14:56"], ["15:09", "15:15"],
+["15:36", "15:56"], ["16:15", "16:23"], ["16:41", "16:47"], ["17:00", "17:14"], ["17:37", "17:42"], ["17:57", "18:08"], ["18:31", "18:37"], ["18:49", "18:55"],
+["19:06", "19:25"], ["19:40", "19:54"], ["20:10", "20:18"], ["20:36", "20:40"], ["20:53", "21:05"], ["21:16", "21:22"], ["21:32", "21:48"], ["22:00", "22:10"],
+["22:23", "22:37"], ["23:13", "23:32"], ["24:00", "24:07"], ["24:22", "24:29"], ["24:43", "24:57"], ["25:10", "25:18"], ["25:53", "25:51"], ["26:58", "27:20"],
+["27:50", "27:59"], ["28:13", "28:35"], ["28:53", "28:59"], ["29:16", "29:21"], ["29:39", "29:46"], ["29:57", "30:07"], ["30:27", "30:34"]]
+video_to_clip("https://youtu.be/AdmCmegtgc8", "22F", "20220908", "court1", "match1", clip_info_list)
 
+# %%
+for i in range(len(clip_info_list)):
+    clip_info = clip_info_list[i]
+
+    start_string = clip_info[0]
+    start_sec = int(start_string.split(":")[0]) * 60 + int(start_string.split(":")[1]) * 1
+    end_string = clip_info[1]
+    end_sec = int(end_string.split(":")[0]) * 60 + int(end_string.split(":")[1]) * 1
+    print("clip" + str(i) + ": " + str(start_sec) + " ~ " + str(end_sec))
 
 # %%
 youtube_link = "https://youtu.be/AdmCmegtgc8"
