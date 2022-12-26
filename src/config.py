2bee79eb9e88cef8b07cdc5428650759638e902a
root <youngjun.woo.data@gmail.com>
2022-12-26 15:05:38 +0000

    edit player_detect

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
 # %%
-DATA_PATH = "/content/drive/My Drive/ALT+TAB/data/"
+DATA_PATH = "/content/drive/MyDrive/ALT+TAB/data/"
 # %%
--- src/player_detect.py
+++ src/player_detect.py
@@ -11,7 +11,7 @@ import glob
 
 # %%
 # download COCO starting checkpoint
-%cd /content/yolov7
+# %cd yolov7
 !wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7_training.pt
 
 # %%
@@ -19,22 +19,9 @@ def player_detect(season, match_date, court_number, match_number):
     max_clip_number = get_maximum_clip(season, match_date, court_number, match_number)
     
     for i in range(1, max_clip_number + 1):
-        clip_path = DATA_PATH + "detect/" + season + "/" + match_date + "/" + court_number + "/" + match_number + "/" + "clip" + str(i) + "/"
-        source_list = glob.glob(clip_path + "*.jpg")
-        !python detect.py --weights /yolov7/yolov7_training.pt --conf 0.1 --source source_path --save-txt --classes 0 38 --project project_path --name player_detect --exist-ok --nosave
+        clip_path = DATA_PATH + "detect/" + season + "/" + match_date + "/" + court_number + "/" + match_number + "/" + "clip" + str(i)
+        source_path = clip_path + "/frames"
+        !python3 detect.py --weights yolov7_training.pt --conf 0.1 --source {source_path} --save-txt --classes 0 38 --project {clip_path} --name player_detect --exist-ok --nosave
 
-
 # %%
-season = "22F"
-match_date = "20220908"
-court_number = "court1"
-match_number = "match1"
-clip_path = DATA_PATH + "detect/" + season + "/" + match_date + "/" + court_number + "/" + match_number + "/" + "clip" + str(1) + "/"
-clip_path
-# %%
-source_list = glob.glob(clip_path + "*.jpg")[:10]
-source_list
-# %%
-%cd /content/yolov7
-!python detect.py --weights /yolov7/yolov7_training.pt --conf 0.1 --source source_list --save-txt --classes 0 38 --project clip_path --name player_detect --exist-ok --nosave
-# %%
+player_detect("22F", "20220908", "court1", "match1")
