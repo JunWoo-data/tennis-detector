502f305cee46b6d7558509e1da30962f712c19f7
root <youngjun.woo.data@gmail.com>
2022-12-26 02:25:21 +0000

    video to clip function

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 # %%
+from moviepy.editor import VideoFileClip
 import pytube
 from pytube import YouTube
 import cv2
@@ -7,6 +8,39 @@ from google.colab.patches import cv2_imshow
 from google.colab import drive
 import numpy as np
 from config import DATA_PATH
+
+# %%
+def video_to_clip(youtube_link, match_date, court_number, match_number, clip_list):
+    # make folder if not exists
+    base_path = DATA_PATH + "detect/" + season + "/" + match_date + "/" + court_number + "/" + match_number + "/"
+    
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+    
+    # download youtube video from link to the folder
+    yt = YouTube(youtube_link)
+    yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download(base_path, "match_video.mp4")
+
+    
+    video_path = base_path + "match_video.mp4"
+    clip = VideoFileClip(video_path)
+
+    for i in range(len(clip_list)):
+        clip = clip_list[i]
+
+        start_string = clip[0]
+        start_sec = int(start_string.split(":")[0]) * 60 + int(start_string.split(":")[1]) * 1
+
+        end_string = clip[1]
+        end_sec = int(end_string.split(":")[0]) * 60 + int(end_string.split(":")[1]) * 1
+
+        clip_number = 1
+        clip_path = base_path + "clip" + str(clip_number) + "/"
+
+        if not os.path.exists(clip_path):
+          os.makedirs(clip_path)
+
+        clip.subclip(start_sec, end_sec).write_videofile(clip_path + "clip" + str(clip_number) + ".mp4")
 
 # %%
 def video_to_frames(youtube_link, season, match_date, court_number, match_number):
